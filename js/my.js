a0f5dc78a5f0aaf0f4156dc7c4f190d29a3f9f5a
Tony <397013181@qq.com>
2015-08-27 17:37:48 +0800

    ifrem嵌入网页控制高度

--- js/my.js
+++ js/my.js
@@ -8,3 +8,13 @@ function moveHead(){
 	var bg = document.getElementById('title_bg');
 	view.style.height = bg.offsetHeight / 2;
 }
+
+
+function iFrameHeight() {   
+	var ifm= document.getElementById("iframepage");   
+	var subWeb = document.frames ? document.frames["iframepage"].document : ifm.contentDocument;   
+	if(ifm != null && subWeb != null) {
+	   ifm.height = subWeb.body.scrollHeight;
+	   ifm.width = subWeb.body.scrollWidth;
+	}   
+}    
