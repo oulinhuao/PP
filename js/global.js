be8b7f5940b2c92a569f950f049221e45692795b
Tony <397013181@qq.com>
2015-08-28 17:37:12 +0800

    引入AngularJs控制输入框显示

--- js/global.js
+++ js/global.js
@@ -11,3 +11,51 @@ function getiframe(height,url){
 					+ height
 					+ 'px"></iframe>'
 }
+
+/**
+ * 获取日期字符串yyyy年mm月dd日
+ */
+function getDate(){
+	var today = new Date();
+	var date = today.getDate();
+	var month = today.getMonth() + 1;
+	var year = today.getFullYear();
+	return year + "年" + month + "月" + date + "日";
+}
+
+/**
+ * 获取星期字符串
+ */
+function getWeek(){
+	var day = new Date().getDay();
+  	var week = "星期日";
+	if(day==0){
+		week = "星期日";
+	} else if(day==0){
+		week = "星期日";
+	}
+	switch(day){
+	case 0:
+		week = "星期日";
+	break;
+  	case 1:
+  		week = "星期一";
+  	break;
+  	case 2:
+  		week = "星期二";
+  	break;
+  	case 3:
+  		week = "星期三";
+  	break;
+  	case 4:
+  		week = "星期四";
+  	break;
+  	case 5:
+  		week = "星期五";
+  	break;
+  	case 6:
+  		week = "星期六";
+  	break;
+	}
+	return week;
+}
